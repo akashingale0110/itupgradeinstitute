a2b6af801983103b68c6d0444e543434b8c9dbd4
akashingale0110 <138219514+akashingale0110@users.noreply.github.com>
2023-07-28 10:15:22 +0530

    Add files via upload
    basic project added.

---
+++ src/main/java/com/itupgradeinstitute/ITUpgradeInstitute.java
@@ -0,0 +1,13 @@
+package com.itupgradeinstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITUpgradeInstitute {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITUpgradeInstitute.class, args);
+	}
+
+}
